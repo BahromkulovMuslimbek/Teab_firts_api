4d299a37ecc01dbb6584ac50da022fb97312b970
BahromkulovMuslimbek <muslimbahromkulov145@gmail.com>
2024-08-08 11:09:57 +0300

    first commit

--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,13 @@ from . import views
 app_name = 'api'
 
 urlpatterns = [
-    path('banner/', views.banner_list, name='banner'),
-    path('product/', views.product_list, name='product'),
-    path('faq/', views.faq_list, name='faq'),
-    path('testimonial/', views.testimonial_list, name='testimonial'),
+    path('banner/list/', views.banner_list, name='banner_list'),
+    path('product/list/', views.product_list, name='product_list'),
+    path('faq/list/', views.faq_list, name='faq_list'),
+    path('testimonial/list/', views.testimonial_list, name='testimonial_list'),
+
+    path('banner/detail/', views.banner_detail, name='banner_detail'),
+    path('product/detail/', views.product_detail, name='product_detail'),
+    path('faq/detail/', views.faq_detail, name='faq_detail'),
+    path('testimonial/detail/', views.testimonial_detail, name='testimonial_detail'),
 ]
--- api/views.py
+++ api/views.py
@@ -30,3 +30,30 @@ def testimonial_list(request):
     testimonials = Testimonial.objects.all()
     serializer_data = TestimonialSerializer(testimonials, many=True)
     return Response(serializer_data.data)
+
+@api_view(['GET'])
+def banner_detail(request):
+    banners = Banner.objects.all()
+    serializer_data = BannerSerializer(banners, many=True)
+    return Response(serializer_data.data)
+
+
+@api_view(['GET'])
+def product_detail(request):
+    products = Product.objects.all()
+    serializer_data = ProductSerializer(products, many=True)
+    return Response(serializer_data.data)
+
+
+@api_view(['GET'])
+def faq_detail(request):
+    banners = Faq.objects.all()
+    serializer_data = FaqSerializer(banners, many=True)
+    return Response(serializer_data.data)
+
+
+@api_view(['GET'])
+def testimonial_detail(request):
+    testimonials = Testimonial.objects.all()
+    serializer_data = TestimonialSerializer(testimonials, many=True)
+    return Response(serializer_data.data)
